3cd3fbdc1f4da2da6878594cceb735f6c81fd7ce
Mostafa Adel <mostafadelgouda@gmail.com>
2024-02-26 16:28:30 +0200

    phonebook step 12

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,11 +6,25 @@ import personService from './services/persons'
 //import create from './services/persons'
 
 
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className='add'>
+      {message}
+    </div>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [newFilter, setNewFilter] = useState('');
+  const [addMessage, setAddMessage] = useState(null)
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -69,6 +83,10 @@ const App = () => {
     
     setNewName('');
     setNewNumber('');
+    setAddMessage('Added ' + newName)
+    setTimeout(() => {
+      setAddMessage(null)
+    }, 5000)
   };
   
   const handleFilterChange = (event) => {
@@ -82,10 +100,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={addMessage} />
       <Filter newFilter = {newFilter} handleFilterChange = {handleFilterChange}/>
       
       <Form newName = {newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange} addName={addName} />
-      <PersonList filteredPersons={filteredPersons} deletePerson = {deleteFromList}/>
+      <PersonList filteredPersons={filteredPersons} deletePerson = {deleteFromList} setAddMessage={setAddMessage}/>
 
     </div>
   );
--- part2/phonebook/src/components/PersonList.jsx
+++ part2/phonebook/src/components/PersonList.jsx
@@ -1,24 +1,34 @@
 import personService from '../services/persons'
 
 
-const FilteredPerson = ({ person , deletePerson}) => {
+const FilteredPerson = ({ person , deletePerson, setAddMessage}) => {
     return <div>{person.name} {person.number}<button onClick={() => {
       if (window.confirm("Do you really want to delete?")) {
-        personService.deletePerson(person.id)
+        const response = personService.deletePerson(person.id)
         deletePerson(person.id)
+        if (response.status >= 200 && response.status < 300) {
+          return response.data;
+        } else {
+          setAddMessage(person.name + ' is already removed from the list')
+          setTimeout(() => {
+            setAddMessage(null)
+          }, 5000)
+        }
+        console.log('ana m4 hena ya jhone')
+        
       }
     
     }}>remove</button></div>;
   };
 
-const PersonList = ({filteredPersons, deletePerson}) => {
+const PersonList = ({filteredPersons, deletePerson, setAddMessage}) => {
     //console.log(handleFilterChange)
     return(
         
       <div>
         <h2>Numbers</h2>
         {filteredPersons.map(person => (
-          <FilteredPerson key={person.id} person={person} deletePerson = {deletePerson}/>
+          <FilteredPerson key={person.id} person={person} deletePerson = {deletePerson} setAddMessage={setAddMessage}/>
         ))}
       </div>
     )
