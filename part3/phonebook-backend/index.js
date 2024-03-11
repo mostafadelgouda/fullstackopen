a8c2a36019fd05cc8c727288eefc96f5df7e27e5
Mostafa Adel <mostafadelgouda@gmail.com>
2024-03-11 12:51:32 +0200

    c

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
+
 const morgan = require('morgan')
 
 const app = express()
 const cors = require('cors')
 const PORT = process.env.PORT || 3001
+
+app.use(express.static('dist'))
 
 app.use(cors())
 app.use(express.json())
